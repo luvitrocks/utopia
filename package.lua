c8d4b9e1e6822c0ca42a07f93cb3c95631a1bd7a
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2014-07-22 17:56:04 +0300

    add package.lua

---
+++ package.lua
@@ -0,0 +1,17 @@
+return {
+	name = "luvit-utopia",
+	version = "0.0.3-beta",
+	description = "High performance middleware framework for Luvit.io",
+	repository = {
+		url = "http://github.com/luvitrocks/luvit-utopia.git",
+	},
+	author = {
+		name = "Dmitri Voronianski",
+		email = "voronianski@gmail.com"
+	},
+	licenses = {"MIT"},
+	devDependencies = {
+		"luvit-test",
+	},
+	main = 'init.lua'
+}
